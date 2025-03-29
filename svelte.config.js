518aee72ddcc4d300c8139a511cafd6e450a768a
Muhamad Rafli <wimpoge@gmail.com>
2025-03-30 04:49:50 +0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // Ignore 500 errors on specific pages
+        if (path === '/sverdle') {
+          return;
+        }
+
+        // For everything else, throw the error
+        throw new Error(message);
+      }
+    }
+  }
 };
