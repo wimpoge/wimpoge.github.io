677318f39a523af04a1fcdda142c066f093858ba
Muhamad Rafli <wimpoge@gmail.com>
2025-04-01 10:36:58 +0700

    update

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,8 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+ 
+injectAnalytics({ mode: dev ? 'development' : 'production' });
